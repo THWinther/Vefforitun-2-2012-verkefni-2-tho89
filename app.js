bfb446a4484273c3076baef1da487ef1e4c92b4f
THWinther <43626565+THWinther@users.noreply.github.com>
2021-02-04 21:39:26 +0000

    basic express/ejs virkni

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 import express from 'express';
 
-console.log('Hello world');
+
+let viewsPath = new URL('./views', import.meta.url).pathname;
+viewsPath = viewsPath.substr(1, viewsPath.length);
+
+let publicURL = new URL('./public', import.meta.url).pathname;
+publicURL = publicURL.substring(1, publicURL.length);
+
+const app = express();
+
+app.locals.importantize = (str) => (`${str}!`);
+
+app.set('view engine', 'ejs');
+app.set('views', viewsPath);
+
+app.use(express.static(publicURL));
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const linkName = `${hostname}:${port}`;
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+
+
+app.listen(port, hostname, () => {
+    app.locals.hostname = linkName;
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
