becdf395d5569589f72da90cf209375d1a22bc38
THWinther <43626565+THWinther@users.noreply.github.com>
2021-02-09 15:21:55 +0000

    ná í gögn frá form

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 
 let viewsPath = new URL('./views', import.meta.url).pathname;
@@ -15,6 +16,8 @@ app.set('view engine', 'ejs');
 app.set('views', viewsPath);
 
 app.use(express.static(publicURL));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -22,6 +25,13 @@ const linkName = `${hostname}:${port}`;
 
 app.get('/', (req, res) => {
     res.render('index')
+});
+
+app.post('/submit-Signature',(req,res)=> {
+    console.log(req.body.name);
+    console.log(req.body.ssn);
+    console.log(req.body.comment);
+    console.log(req.body.list);
 });
 
 
